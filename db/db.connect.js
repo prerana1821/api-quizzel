cfd987102e3f6fb1a7bf25bd26ab1c6df14e6877
Prerana Nawar <>
2021-05-25 06:39:18 +0000

    feat: completed database integration

---
+++ db/db.connect.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const uri = process.env['uri'];
+const initializeDatabase = async () => {
+  console.log('Hello');
+  try {
+    const connection = await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    if (connection) {
+      console.log('Connected Successfully');
+    }
+  } catch (error) {
+    console.log('Connection Failed', error);
+  }
+}
+
+module.exports = { initializeDatabase };
--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { initializeDatabase } = require("./db/db.connect");
 const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+initializeDatabase();
 
 app.get('/', (req, res) => {
-  res.send('Hello Express app!')
+  res.send('Hello Quizzel!')
 });
 
-app.listen(3000, () => {
-  console.log('server started');
+app.listen(PORT, () => {
+  console.log('server started', PORT);
 });
